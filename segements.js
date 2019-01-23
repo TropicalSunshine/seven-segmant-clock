7d22b19ec6b190399bc91d9671f56eb8b8bdfc0a
Jason Liu <jasonliu21396@gmail.com>
2019-01-23 15:36:00 -0800

    intial commit

---
+++ segements.js
@@ -0,0 +1,9 @@
+var seg_0 = {
+    a:document.getElementById("seg-0").getElementById("seg-0a"),
+    b:document.getElementById("seg-0").getElementById("seg-0b"),
+    c:document.getElementById("seg-0").getElementById("seg-0b"),
+    d:document.getElementById("seg-0").getElementById("seg-0b"),
+    e:document.getElementById("seg-0").getElementById("seg-0b"),
+    f:document.getElementById("seg-0").getElementById("seg-0b"),
+    g:document.getElementById("seg-0").getElementById("seg-0b"),
+}
--- seven.js
+++ seven.js
@@ -11,22 +11,20 @@ to-do
 //B ((0x7E >> 6) & 1)
 //C ((0x7E >> 6) & 1)
 //D ((0x7E >> 6) & 1)
-//E ((0x7E >> 6) & 1)
+//E ((0x7E >> 6) & 1{)
 //F ((0x7E >> 6) & 1)
 //G ((0x7E >> 6) & 1)
 //if 1 turn on 
 //if 0 turn off     
 
-
 const OFFCOLOR = "rgba(141, 1, 1, 0.3)";
 const ONCOLOR = "rgba(245, 21, 21, 1)";
 var colon = document.getElementById("colon-0").getElementById("g0"); 
 
-var seg_0 = document.getElementById("seg-0").getElementById("seg-0a");
-
-seg_0.style.fill = OFFCOLOR
-
-
+var seg_0 = getElements(0);
+var seg_1 = getElements(1);
+var seg_2 = getElements(2);
+var seg_3 = getElements(2);
 
 window.onload = function (){
     //make the colon blink between clock
@@ -45,4 +43,13 @@ var blink = function(){
 
 var elapse = function(){
     
+}
+
+function getElements(seg_num){
+    seg = ["a","b","c","d","e","f","g"];
+    result = {};
+    for(let i=0; i<7;i++){
+        result[seg[i]] = document.getElementById("seg-"+seg_num).getElementById("seg-"+seg_num+seg[i]);
+    }
+    return result;
 }
